3a9de9c37017cb9806cecad2863e3f1bfb4c297f
Marios Ioannidis <ioanmar80@gmail.com>
2018-08-01 18:46:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
-  fetch('https://anapioficeandfire.com/api/books')
+  fetch('https://anapioficeandfire.com/api/books?pageSize=1000')
   .then(resp => resp.json())
   .then(json => console.log(json));
 }
